06c96ccf51b09f8359110176175bc4f8201ef9a1
JJ <jj.vega86@gmail.com>
2021-04-16 11:03:39 -0400

    Added serializer class for song model; added class-based view and GET/POST endpoint functions

---
+++ music_library/songs/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import Song
+
+
+class SongSerializer(serializers.ModelSerializer):
+    # defining fields allows ModelSerializer to match list of fields to model
+    # note added id even though it isn't explcitely written in model. Database auto-generates
+    class Meta:
+        model = Song
+        fields = ['id', 'title', 'artist', 'album', 'release_date']
--- music_library/songs/views.py
+++ music_library/songs/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from .models import Song
+from .serializers import SongSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+class SongList(APIView):
+
+    def get(self, request):
+        # query for all Song records in the database
+        # convert QuerySet of song objects into a list of dictionaries
+        # pass serializer.data into Response(), which converts the list of dictionaries into a JSON string literal
+        # then returns the response to the requestor (client who hit endpoint)
+        songs = Song.objects.all()
+        serializer = SongSerializer(songs, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = SongSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
