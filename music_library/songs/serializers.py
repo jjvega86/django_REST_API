a616f6901bc6390c61390375eb2f9bc8ee18b74e
JJ <jj.vega86@gmail.com>
2021-04-16 12:00:40 -0400

    Added SongDetail class-based view with function to get object and endpoints for get by id, put, and delete

--- music_library/songs/serializers.py
+++ music_library/songs/serializers.py
@@ -4,7 +4,7 @@ from .models import Song
 
 class SongSerializer(serializers.ModelSerializer):
     # defining fields allows ModelSerializer to match list of fields to model
-    # note added id even though it isn't explcitely written in model. Database auto-generates
+    # note added id even though it isn't explcitely written in model. Database auto-generates the id
     class Meta:
         model = Song
         fields = ['id', 'title', 'artist', 'album', 'release_date']
--- music_library/songs/urls.py
+++ music_library/songs/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('songs/', views.SongList.as_view())
+    path('songs/', views.SongList.as_view()),
+    path('songs/<int:pk>/', views.SongDetail.as_view())
 ]
